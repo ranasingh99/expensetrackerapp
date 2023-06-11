8330647b821acccfaddfbfdd3bf6578e22f73eed
ranasingh99 <sanketverma852@gmail.com>
2023-06-11 09:48:07 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let currentbalence = document.getElementById('yourmoney');
+currentbalence.style.color = 'green';
+let income = document.getElementById('income');
+income.style.color = 'green';
+let expense = document.getElementById('expense');
+expense.style.color = 'red';
